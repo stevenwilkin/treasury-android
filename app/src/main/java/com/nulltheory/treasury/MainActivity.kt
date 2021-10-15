012bd857720703bb7ae5cf31543ac213518068be
Steven Wilkin <stevenwilkin@gmail.com>
2021-10-15 21:42:13 +0700

    Skeleton websocket client

--- app/src/main/java/com/nulltheory/treasury/MainActivity.kt
+++ app/src/main/java/com/nulltheory/treasury/MainActivity.kt
@@ -2,10 +2,48 @@ package com.nulltheory.treasury
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+import java.lang.Exception
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+    lateinit var client: WebSocketClient
+    val wsUrl: String = BuildConfig.wsUrl
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        client = object : WebSocketClient(URI(wsUrl)) {
+            override fun onOpen(handshakedata: ServerHandshake?) {
+                Log.d("socket", "onOpen")
+            }
+
+            override fun onMessage(message: String?) {
+                Log.d("socket", "onMessage: $message")
+            }
+
+            override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                Log.d("socket", "onClose")
+            }
+
+            override fun onError(ex: Exception?) {
+                Log.d("socket", "onError: ${ex?.message}")
+            }
+
+        }
+
+        client.connect()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        client.close()
     }
 }
